08cc8dc3d2c850e1aafe889048805ef3687befc5
Adrian Bolinger <adrian.bolinger@me.com>
2018-03-04 22:14:51 -0500

    Initial Commit

--- StackViewDemo/ViewController.swift
+++ StackViewDemo/ViewController.swift
@@ -9,17 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  
+  @IBOutlet weak var buttonOne: UIButton!
+  @IBOutlet weak var buttonTwo: UIButton!
+  @IBOutlet weak var buttonThree: UIButton!
+  @IBOutlet weak var buttonFour: UIButton!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  
+  // MARK: - IBActions
+  
+  @IBAction func buttonAction(_ sender: UIButton) {
+    switch sender {
+    case buttonOne:
+      print("Button One")
+    case buttonTwo:
+      print("Button Two")
+    case buttonThree:
+      print("Button Three")
+    case buttonFour:
+      print("Button Four")
+    default:
+      return
+    }
+    
   }
-
-
 }
 
