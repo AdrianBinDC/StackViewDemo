006d14f25ca5a9ac2220d8c73c52eee0b0459d1b
Adrian Bolinger <adrian.bolinger@me.com>
2018-03-04 22:25:43 -0500

    Added Colors to UIButtons

--- StackViewDemo/ViewController.swift
+++ StackViewDemo/ViewController.swift
@@ -18,6 +18,13 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    let colors = [UIColor.red, UIColor.orange, UIColor.yellow, UIColor.green]
+    let buttons = [buttonOne, buttonTwo, buttonThree, buttonFour].flatMap{$0}
+    let buttonDict = zip(buttons, colors)
+    
+    for (button, color) in buttonDict {
+      button.backgroundColor = color
+    }
   }
   
   // MARK: - IBActions
